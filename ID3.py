8c0ffb103c39da8981f2c68e87d8fa04f8ff0472
Edward Wong <edward4wong@gmail.com>
2015-04-27 20:32:37 -0700

    prototype for node we'll use for the decision tree, may need more things and may need to take out some things.

--- ID3.py
+++ ID3.py
@@ -6,6 +6,31 @@ import random
 training_set = []
 test_set = []
 
+class Node:
+    def __init__(self,data):
+        self.left = None
+        self.right = None
+        self.data = data
+        self.threshold_indices = -1
+        self.threshold = -1
+
+    def setThresholdIndices(self, index):
+    	self.threshold_indices = index
+    def setThreshold(self, val):
+    	self.threshold = val
+    def setLeft(self, elem):
+    	self.left = elem
+    def setright(self, elem):
+    	self.right = elem
+    def getRight(self):
+    	return self.right
+    def getLeft(self):
+    	return self.left
+    def getData(self):
+    	return self.data
+
+
+
 def load(fn, ds):
 	f = open(fn, "r")
 	for line in f:
@@ -16,7 +41,13 @@ def load(fn, ds):
 		data = (features, int(tokens[len(tokens) - 1]))
 		ds.append(data)
 
+def calc_entropy(data):
+	count = [0,0,0,0] 
+	# really only need indices 1,2,3 as those are the only labels
+
 load("hw3train.txt", training_set)
 load("hw3test.txt", test_set)
 
-print training_set[0]
+sort_test = sorted(training_set, key=lambda tup: tup[0][0])
+root = Node(training_set)
+print root.getData()
