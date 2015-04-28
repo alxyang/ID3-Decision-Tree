7e41cea21e38a4d3b1fa5ccc00fa1b344d25fcef
Edward Wong <edward4wong@gmail.com>
2015-04-27 20:51:06 -0700

    added code to calc entropy given a data set and given that there are only 3 possible labels (1,2,3

--- ID3.py
+++ ID3.py
@@ -42,12 +42,23 @@ def load(fn, ds):
 		ds.append(data)
 
 def calc_entropy(data):
-	count = [0,0,0,0] 
+	count = [0,0,0,0]
+	total = float(0)
 	# really only need indices 1,2,3 as those are the only labels
+	for (features,label) in data:
+		count[label] = count[label] + 1
+		total = total + 1
+	entropy = float(0)
+	for c in count:
+		if c == 0:
+			continue
+		prob = c / total
+		entropy = entropy + prob * math.log(prob)
+	entropy = entropy * -1
+	return entropy
 
 load("hw3train.txt", training_set)
 load("hw3test.txt", test_set)
 
 sort_test = sorted(training_set, key=lambda tup: tup[0][0])
-root = Node(training_set)
-print root.getData()
+print calc_entropy([(0,1),(0,2), (0,3)])
